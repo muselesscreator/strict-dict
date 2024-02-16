a8c049f7ae09161f2d11180db62d1c87ac8d9e44
muselesscreator <bwarzesk@gmail.com>
2024-02-15 19:17:13 -0500

    feat: releases

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+/* v8 ignore start */
+export { default as StrictContext } from './StrictContext';
+export { default as StrictDict } from './StrictDict';
+export { default as keyStore } from './keyStore';
+export { default as useStrictDict } from './useStrictDict';
+export { default as useKeyStore } from './useKeyStore';
